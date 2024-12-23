07abf1e5bd895334ba7358a0458b6c9eff709d2c
sky <1448039785@qq.com>
2024-12-23 22:06:08 +0800

    使用spring cache缓存套餐数据

--- reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -4,12 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @ServletComponentScan
 @Slf4j
 @SpringBootApplication
 @EnableTransactionManagement   // 开启事务注解
+@EnableCaching  // 开启SpringCache注解方式的缓存功能
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
